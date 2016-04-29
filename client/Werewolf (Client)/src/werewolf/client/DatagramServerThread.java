e672c0cb6804a9fbc15fdd7b7b25f48b9a75efce
Adin Baskoro <master.adin@gmail.com>
2016-04-29 19:43:48 +0700

    wowow

---
+++ client/Werewolf (Client)/src/werewolf/client/DatagramServerThread.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package werewolf.client;
+
+/**
+ *
+ * @author adinb
+ */
+public class DatagramServerThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("This is server thread");
+    }
+    
+}
